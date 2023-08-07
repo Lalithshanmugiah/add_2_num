d49f6a1050fedc83d45a2466a1fdf730ee20bb50
Lalithshanmugiah <89589081+Lalithshanmugiah@users.noreply.github.com>
2023-08-07 12:06:18 +0530

    add two numbers

---
+++ script.js
@@ -0,0 +1,20 @@
+// add two numbers
+
+let num1 = 5
+let num2 = 5
+
+const num = num1 + num2;
+
+//display sum
+console.log("the sum "+num);
+
+//add two num enter by user
+// store input numbers
+const numa = parseInt(prompt('Enter the first number '));
+const numb = parseInt(prompt('Enter the second number '));
+
+//add two numbers
+const sum = numa + numb;
+
+// display the sum
+console.log(`The sum of ${numa} and ${numb} is ${sum}`);
